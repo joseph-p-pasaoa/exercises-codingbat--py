d153636a65bec8269d57ff8a59c09991433a52b7
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-10-10 15:41:17 -0400

    completed four more warmup1 exercises

--- warmup1.py
+++ warmup1.py
@@ -58,3 +58,114 @@ def sum_double(a, b):
 
 
 
+# DIFF21
+# Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+#
+# diff21(19) → 2
+# diff21(10) → 11
+# diff21(21) → 0
+
+def diff21(n):
+  diff21 = abs(n - 21)
+
+  if n > 21:
+    return diff21 * 2
+  else:
+    return diff21
+
+# diff21(19) → 2	2	OK	
+# diff21(10) → 11	11	OK	
+# diff21(21) → 0	0	OK	
+# diff21(22) → 2	2	OK	
+# diff21(25) → 8	8	OK	
+# diff21(30) → 18	18	OK	
+# diff21(0) → 21	21	OK	
+# diff21(1) → 20	20	OK	
+# diff21(2) → 19	19	OK	
+# diff21(-1) → 22	22	OK	
+# diff21(-2) → 23	23	OK	
+# diff21(50) → 58	58	OK
+
+
+
+# PARROT_TROUBLE
+# We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+#
+# parrot_trouble(True, 6) → True
+# parrot_trouble(True, 7) → False
+# parrot_trouble(False, 6) → False
+
+def parrot_trouble(talking, hour):
+  isTroubleTime = hour < 7 or hour > 20
+  return talking and isTroubleTime
+
+# parrot_trouble(True, 6) → True	True	OK	
+# parrot_trouble(True, 7) → False	False	OK	
+# parrot_trouble(False, 6) → False	False	OK	
+# parrot_trouble(True, 21) → True	True	OK	
+# parrot_trouble(False, 21) → False	False	OK	
+# parrot_trouble(False, 20) → False	False	OK	
+# parrot_trouble(True, 23) → True	True	OK	
+# parrot_trouble(False, 23) → False	False	OK	
+# parrot_trouble(True, 20) → False	False	OK	
+# parrot_trouble(False, 12) → False	False	OK
+
+
+
+# MAKES10
+# Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+#
+# makes10(9, 10) → True
+# makes10(9, 9) → False
+# makes10(1, 9) → True
+
+def makes10(a, b):
+  return (a + b == 10) or (a == 10) or (b == 10)
+
+# makes10(9, 10) → True	True	OK	
+# makes10(9, 9) → False	False	OK	
+# makes10(1, 9) → True	True	OK	
+# makes10(10, 1) → True	True	OK	
+# makes10(10, 10) → True	True	OK	
+# makes10(8, 2) → True	True	OK	
+# makes10(8, 3) → False	False	OK	
+# makes10(10, 42) → True	True	OK	
+# makes10(12, -2) → True	True	OK
+
+
+
+# NEAR_HUNDRED
+# Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+#
+# near_hundred(93) → True
+# near_hundred(90) → True
+# near_hundred(89) → False
+
+def near_hundred(n):
+  distFrom100 = abs(100 - n)
+  distFrom200 = abs(200 - n)
+
+  return (distFrom100 <= 10) or (distFrom200 <= 10)
+
+# near_hundred(93) → True	True	OK	
+# near_hundred(90) → True	True	OK	
+# near_hundred(89) → False	False	OK	
+# near_hundred(110) → True	True	OK	
+# near_hundred(111) → False	False	OK	
+# near_hundred(121) → False	False	OK	
+# near_hundred(-101) → False	False	OK	
+# near_hundred(-209) → False	False	OK	
+# near_hundred(190) → True	True	OK	
+# near_hundred(209) → True	True	OK	
+# near_hundred(0) → False	False	OK	
+# near_hundred(5) → False	False	OK	
+# near_hundred(-50) → False	False	OK	
+# near_hundred(191) → True	True	OK	
+# near_hundred(189) → False	False	OK	
+# near_hundred(200) → True	True	OK	
+# near_hundred(210) → True	True	OK	
+# near_hundred(211) → False	False	OK	
+# near_hundred(290) → False	False	OK
+
+
+
