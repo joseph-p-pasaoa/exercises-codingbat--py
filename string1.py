4d7b1b6cb2c0e5fbe6b62c044ce53a58f56d4498
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-10-11 23:21:35 -0400

    completed 4 more string1 exercises

--- string1.py
+++ string1.py
@@ -72,3 +72,92 @@ def make_tags(tag, word):
 
 
 
+# MAKE_OUT_WORD
+# Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is
+# in the middle of the out string, e.g. "<<word>>".
+#
+# make_out_word('<<>>', 'Yay') → '<<Yay>>'
+# make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
+# make_out_word('[[]]', 'word') → '[[word]]'
+
+def make_out_word(out, word):
+  return out[:2] + word + out[2:]
+
+# make_out_word('<<>>', 'Yay') → '<<Yay>>'	'<<Yay>>'	OK	
+# make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'	'<<WooHoo>>'	OK	
+# make_out_word('[[]]', 'word') → '[[word]]'	'[[word]]'	OK	
+# make_out_word('HHoo', 'Hello') → 'HHHellooo'	'HHHellooo'	OK	
+# make_out_word('abyz', 'YAY') → 'abYAYyz'	'abYAYyz'	OK	
+# other tests OK
+
+
+
+# EXTRA_END
+# Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+# The string length will be at least 2.
+#
+# extra_end('Hello') → 'lololo'
+# extra_end('ab') → 'ababab'
+# extra_end('Hi') → 'HiHiHi'
+
+def extra_end(str):
+  last2 = str[-2:]
+  output = ''
+  for _ in range(3):
+    output += last2
+  return output
+
+# extra_end('Hello') → 'lololo'	'lololo'	OK	
+# extra_end('ab') → 'ababab'	'ababab'	OK	
+# extra_end('Hi') → 'HiHiHi'	'HiHiHi'	OK	
+# extra_end('Candy') → 'dydydy'	'dydydy'	OK	
+# extra_end('Code') → 'dedede'	'dedede'	OK	
+# other tests OK
+
+
+
+# FIRST_TWO
+# Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
+# If the string is shorter than length 2, return whatever there is, so "X" yields "X", and
+# the empty string "" yields the empty string "".
+#
+# first_two('Hello') → 'He'
+# first_two('abcdefg') → 'ab'
+# first_two('ab') → 'ab'
+
+def first_two(str):
+  return str[:2]
+
+# first_two('Hello') → 'He'	'He'	OK	
+# first_two('abcdefg') → 'ab'	'ab'	OK	
+# first_two('ab') → 'ab'	'ab'	OK	
+# first_two('a') → 'a'	'a'	OK	
+# first_two('') → ''	''	OK	
+# first_two('Kitten') → 'Ki'	'Ki'	OK	
+# first_two('hi') → 'hi'	'hi'	OK	
+# first_two('hiya') → 'hi'	'hi'	OK	
+# other tests OK
+
+
+
+# FIRST_HALF
+# Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+#
+# first_half('WooHoo') → 'Woo'
+# first_half('HelloThere') → 'Hello'
+# first_half('abcdef') → 'abc'
+
+def first_half(str):
+  return str[:len(str) / 2]
+
+# first_half('WooHoo') → 'Woo'	'Woo'	OK	
+# first_half('HelloThere') → 'Hello'	'Hello'	OK	
+# first_half('abcdef') → 'abc'	'abc'	OK	
+# first_half('ab') → 'a'	'a'	OK	
+# first_half('') → ''	''	OK	
+# first_half('0123456789') → '01234'	'01234'	OK	
+# first_half('kitten') → 'kit'	'kit'	OK	
+# other tests OK
+
+
+
