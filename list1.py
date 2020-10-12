34f2639d7d083f862a705565eef643f9fb037d22
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-10-11 23:33:56 -0400

    completed 4 more list1 exercises

--- list1.py
+++ list1.py
@@ -70,3 +70,82 @@ def make_pi():
 
 
 
+# COMMON_END
+# Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+#
+# common_end([1, 2, 3], [7, 3]) → True
+# common_end([1, 2, 3], [7, 3, 2]) → False
+# common_end([1, 2, 3], [1, 3]) → True
+
+def common_end(a, b):
+  return a[0] == b[0] or a[-1] == b[-1]
+
+# common_end([1, 2, 3], [7, 3]) → True	True	OK	
+# common_end([1, 2, 3], [7, 3, 2]) → False	False	OK	
+# common_end([1, 2, 3], [1, 3]) → True	True	OK	
+# common_end([1, 2, 3], [1]) → True	True	OK	
+# common_end([1, 2, 3], [2]) → False	False	OK	
+# other tests OK
+
+
+
+# SUM3
+# Given an array of ints length 3, return the sum of all the elements.
+#
+# sum3([1, 2, 3]) → 6
+# sum3([5, 11, 2]) → 18
+# sum3([7, 0, 0]) → 7
+
+def sum3(nums):
+  return sum(nums)
+
+# sum3([1, 2, 3]) → 6	6	OK	
+# sum3([5, 11, 2]) → 18	18	OK	
+# sum3([7, 0, 0]) → 7	7	OK	
+# sum3([1, 2, 1]) → 4	4	OK	
+# sum3([1, 1, 1]) → 3	3	OK	
+# sum3([2, 7, 2]) → 11	11	OK
+
+
+
+# ROTATE_LEFT3
+# Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+# rotate_left3([1, 2, 3]) → [2, 3, 1]
+# rotate_left3([5, 11, 9]) → [11, 9, 5]
+# rotate_left3([7, 0, 0]) → [0, 0, 7]
+
+def rotate_left3(nums):
+  return nums[1:] + nums[:1]
+
+# rotate_left3([1, 2, 3]) → [2, 3, 1]	[2, 3, 1]	OK	
+# rotate_left3([5, 11, 9]) → [11, 9, 5]	[11, 9, 5]	OK	
+# rotate_left3([7, 0, 0]) → [0, 0, 7]	[0, 0, 7]	OK	
+# rotate_left3([1, 2, 1]) → [2, 1, 1]	[2, 1, 1]	OK	
+# rotate_left3([0, 0, 1]) → [0, 1, 0]	[0, 1, 0]	OK	
+# other tests OK
+
+
+
+# REVERSE3
+# Given an array of ints length 3, return a new array with the elements in reverse order,
+# so {1, 2, 3} becomes {3, 2, 1}.
+#
+# reverse3([1, 2, 3]) → [3, 2, 1]
+# reverse3([5, 11, 9]) → [9, 11, 5]
+# reverse3([7, 0, 0]) → [0, 0, 7]
+
+def reverse3(nums):
+  return nums[::-1]
+
+# reverse3([1, 2, 3]) → [3, 2, 1]	[3, 2, 1]	OK	
+# reverse3([5, 11, 9]) → [9, 11, 5]	[9, 11, 5]	OK	
+# reverse3([7, 0, 0]) → [0, 0, 7]	[0, 0, 7]	OK	
+# reverse3([2, 1, 2]) → [2, 1, 2]	[2, 1, 2]	OK	
+# reverse3([1, 2, 1]) → [1, 2, 1]	[1, 2, 1]	OK	
+# reverse3([2, 11, 3]) → [3, 11, 2]	[3, 11, 2]	OK	
+# reverse3([0, 6, 5]) → [5, 6, 0]	[5, 6, 0]	OK	
+# reverse3([7, 2, 3]) → [3, 2, 7]	[3, 2, 7]	OK	
+# other tests OK 
+
+
