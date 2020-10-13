37bfe195ee054d271e9ec2623a5157f9d519d0cb
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-10-13 00:30:06 -0400

    complete 3 more list2 exercises, complete list2, complete all python exercises

--- list2.py
+++ list2.py
@@ -109,3 +109,114 @@ def centered_average(nums):
 
 
 
+# SUM13
+# Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13
+# is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+#
+# sum13([1, 2, 2, 1]) → 6
+# sum13([1, 1]) → 2
+# sum13([1, 2, 2, 1, 13]) → 6
+
+def sum13(nums):
+  sum = 0
+  skip = False
+  for i in range(len(nums)):
+    value = nums[i]
+    if value == 13:
+      skip = True
+    elif skip:
+      skip = False
+    else:
+      sum += value
+  
+  return sum
+
+# sum13([1, 2, 2, 1]) → 6	6	OK	
+# sum13([1, 1]) → 2	2	OK	
+# sum13([1, 2, 2, 1, 13]) → 6	6	OK	
+# sum13([1, 2, 13, 2, 1, 13]) → 4	4	OK	
+# sum13([13, 1, 2, 13, 2, 1, 13]) → 3	3	OK	
+# sum13([]) → 0	0	OK	
+# sum13([13]) → 0	0	OK	
+# sum13([13, 13]) → 0	0	OK	
+# sum13([13, 0, 13]) → 0	0	OK	
+# sum13([13, 1, 13]) → 0	0	OK	
+# sum13([5, 7, 2]) → 14	14	OK	
+# sum13([5, 13, 2]) → 5	5	OK	
+# sum13([0]) → 0	0	OK	
+# sum13([13, 0]) → 0	0	OK	
+# other tests OK
+
+
+
+# SUM67
+# Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6
+# and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+#
+# sum67([1, 2, 2]) → 5
+# sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+# sum67([1, 1, 6, 7, 2]) → 4
+
+def sum67(nums):
+  sum = 0
+  is_adding = True
+  for num in nums:
+    if is_adding:
+      if num == 6:
+        is_adding = False
+      else:
+        sum += num
+    elif num == 7:
+      is_adding = True
+
+  return sum
+
+# sum67([1, 2, 2]) → 5	5	OK	
+# sum67([1, 2, 2, 6, 99, 99, 7]) → 5	5	OK	
+# sum67([1, 1, 6, 7, 2]) → 4	4	OK	
+# sum67([1, 6, 2, 2, 7, 1, 6, 99, 99, 7]) → 2	2	OK	
+# sum67([1, 6, 2, 6, 2, 7, 1, 6, 99, 99, 7]) → 2	2	OK	
+# sum67([2, 7, 6, 2, 6, 7, 2, 7]) → 18	18	OK	
+# sum67([2, 7, 6, 2, 6, 2, 7]) → 9	9	OK	
+# sum67([1, 6, 7, 7]) → 8	8	OK	
+# sum67([6, 7, 1, 6, 7, 7]) → 8	8	OK	
+# sum67([6, 8, 1, 6, 7]) → 0	0	OK	
+# sum67([]) → 0	0	OK	
+# sum67([6, 7, 11]) → 11	11	OK	
+# sum67([11, 6, 7, 11]) → 22	22	OK	
+# sum67([2, 2, 6, 7, 7]) → 11	11	OK	
+# other tests OK
+
+
+
+# HAS22
+# Given an array of ints, return True if the array contains a 2 next to a 2 somewhere.
+#
+# has22([1, 2, 2]) → True
+# has22([1, 2, 1, 2]) → False
+# has22([2, 1, 2]) → False
+
+def has22(nums):
+  for i in range(len(nums) - 1):
+    curr, next = nums[i], nums[i + 1]
+    
+    if curr == 2 and next == 2:
+      return True
+      
+  return False
+
+# has22([1, 2, 2]) → True	True	OK	
+# has22([1, 2, 1, 2]) → False	False	OK	
+# has22([2, 1, 2]) → False	False	OK	
+# has22([2, 2, 1, 2]) → True	True	OK	
+# has22([1, 3, 2]) → False	False	OK	
+# has22([1, 3, 2, 2]) → True	True	OK	
+# has22([2, 3, 2, 2]) → True	True	OK	
+# has22([4, 2, 4, 2, 2, 5]) → True	True	OK	
+# has22([1, 2]) → False	False	OK	
+# has22([2, 2]) → True	True	OK	
+# has22([2]) → False	False	OK	
+# has22([]) → False	False	OK	
+# has22([3, 3, 2, 2]) → True	True	OK	
+# has22([5, 2, 5, 2]) → False	False	OK	
+# other tests OK
