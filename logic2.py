28260e4a8b016a81c1e917fa338b877a3b2a0fcc
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-10-12 23:53:30 -0400

    complete 3 more logic2 exercises, complete logic2

--- logic2.py
+++ logic2.py
@@ -173,3 +173,135 @@ def fix_teen(n):
 
 
 
+# ROUND_SUM
+# For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is
+# 5 or more, so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its
+# rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of
+# their rounded values. To avoid code repetition, write a separate helper "def round10(num):" and call
+# it 3 times. Write the helper entirely below and at the same indent level as round_sum().
+#
+# round_sum(16, 17, 18) → 60
+# round_sum(12, 13, 14) → 30
+# round_sum(6, 4, 4) → 10
+
+def round_sum(a, b, c):
+  sum = 0
+  for num in [a, b, c]:
+    sum += round10(num)
+
+  return sum
+
+
+def round10(num):
+  num = float(num)
+
+  if num % 10 < 5:
+    return int(math.floor(num / 10) * 10)
+  else:
+    return int(math.ceil(num / 10) * 10)
+
+# round_sum(16, 17, 18) → 60	60	OK	
+# round_sum(12, 13, 14) → 30	30	OK	
+# round_sum(6, 4, 4) → 10	10	OK	
+# round_sum(4, 6, 5) → 20	20	OK	
+# round_sum(4, 4, 6) → 10	10	OK	
+# round_sum(9, 4, 4) → 10	10	OK	
+# round_sum(0, 0, 1) → 0	0	OK	
+# round_sum(0, 9, 0) → 10	10	OK	
+# round_sum(10, 10, 19) → 40	40	OK	
+# round_sum(20, 30, 40) → 90	90	OK	
+# round_sum(45, 21, 30) → 100	100	OK	
+# round_sum(23, 11, 26) → 60	60	OK	
+# round_sum(23, 24, 25) → 70	70	OK	
+# round_sum(25, 24, 25) → 80	80	OK	
+# round_sum(23, 24, 29) → 70	70	OK	
+# round_sum(11, 24, 36) → 70	70	OK	
+# round_sum(24, 36, 32) → 90	90	OK	
+# round_sum(14, 12, 26) → 50	50	OK	
+# round_sum(12, 10, 24) → 40	40	OK	
+# other tests OK
+
+
+
+# CLOSE_FAR
+# Given three ints, a b c, return True if one of b or c is "close" (differing from a by at most 1),
+# while the other is "far", differing from both other values by 2 or more. Note: abs(num) computes
+# the absolute value of a number.
+#
+# close_far(1, 2, 10) → True
+# close_far(1, 2, 3) → False
+# close_far(4, 1, 3) → True
+
+def close_far(a, b, c):
+  ab_diff = abs(a - b)
+  ac_diff = abs(a - c)
+  bc_diff = abs(b - c)
+  
+  if (ab_diff <= 1 and ac_diff >= 2):
+    return bc_diff >= 2  
+  elif (ab_diff >= 2 and ac_diff <= 1):
+    return bc_diff >= 2
+  else:
+    return False
+
+# close_far(1, 2, 10) → True	True	OK	
+# close_far(1, 2, 3) → False	False	OK	
+# close_far(4, 1, 3) → True	True	OK	
+# close_far(4, 5, 3) → False	False	OK	
+# close_far(4, 3, 5) → False	False	OK	
+# close_far(-1, 10, 0) → True	True	OK	
+# close_far(0, -1, 10) → True	True	OK	
+# close_far(10, 10, 8) → True	True	OK	
+# close_far(10, 8, 9) → False	False	OK	
+# close_far(8, 9, 10) → False	False	OK	
+# close_far(8, 9, 7) → False	False	OK	
+# close_far(8, 6, 9) → True	True	OK	
+# other tests OK
+
+
+
+# MAKE_CHOCOLATE
+# We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars
+# (5 kilos each). Return the number of small bars to use, assuming we always use big bars before
+# small bars. Return -1 if it can't be done.
+#
+# make_chocolate(4, 1, 9) → 4
+# make_chocolate(4, 1, 10) → -1
+# make_chocolate(4, 1, 7) → 2
+
+def make_chocolate(small, big, goal):
+  max_big_wanted = math.floor(goal / 5)
+  
+  bigs_applied = min(max_big_wanted, big)
+  smalls_needed = goal - bigs_applied * 5
+  
+  if small >= smalls_needed:
+    return int(min(small, smalls_needed))
+  else:
+    return -1
+
+# make_chocolate(4, 1, 9) → 4	4	OK	
+# make_chocolate(4, 1, 10) → -1	-1	OK	
+# make_chocolate(4, 1, 7) → 2	2	OK	
+# make_chocolate(6, 2, 7) → 2	2	OK	
+# make_chocolate(4, 1, 5) → 0	0	OK	
+# make_chocolate(4, 1, 4) → 4	4	OK	
+# make_chocolate(5, 4, 9) → 4	4	OK	
+# make_chocolate(9, 3, 18) → 3	3	OK	
+# make_chocolate(3, 1, 9) → -1	-1	OK	
+# make_chocolate(1, 2, 7) → -1	-1	OK	
+# make_chocolate(1, 2, 6) → 1	1	OK	
+# make_chocolate(1, 2, 5) → 0	0	OK	
+# make_chocolate(6, 1, 10) → 5	5	OK	
+# make_chocolate(6, 1, 11) → 6	6	OK	
+# make_chocolate(6, 1, 12) → -1	-1	OK	
+# make_chocolate(6, 1, 13) → -1	-1	OK	
+# make_chocolate(6, 2, 10) → 0	0	OK	
+# make_chocolate(6, 2, 11) → 1	1	OK	
+# make_chocolate(6, 2, 12) → 2	2	OK	
+# make_chocolate(60, 100, 550) → 50	50	OK	
+# make_chocolate(1000, 1000000, 5000006) → 6	6	OK	
+# make_chocolate(7, 1, 12) → 7	7	OK	
+# make_chocolate(7, 1, 13) → -1	-1	OK	
+# make_chocolate(7, 2, 13) → 3	3	OK	
+# other tests OK
